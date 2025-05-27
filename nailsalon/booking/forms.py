4a71bfeb60a9901c70c8350946d76fb4ec04fa00
HiperDxD <139216015+HiperDxD@users.noreply.github.com>
2025-05-27 20:56:04 +0300

    Changes were made for home/main page and service widgets

--- nailsalon/booking/forms.py
+++ nailsalon/booking/forms.py
@@ -2,7 +2,15 @@ from django import forms
 from .models import Appointment
 
 class AppointmentForm(forms.ModelForm):
+    TIME_CHOICES = [(f"{h:02d}:00", f"{h:02d}:00") for h in range(10, 21)]  # 10:00 to 20:00
+    time = forms.ChoiceField(choices=TIME_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
     class Meta:
         model = Appointment
         fields = ['name', 'email', 'service', 'date', 'time']
-       
+        widgets = {
+            'date': forms.DateInput(attrs={
+                'type': 'text',
+                'placeholder': 'Select a date',
+                'class': 'datepicker'  # optional, for better targeting
+            })
+        }
